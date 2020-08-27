2ed1e8d55d6a49d6b8e4341acd09285956ec8018
Mark Suscens <mark.suscens@gmail.com>
2020-08-27 16:32:51 +0100

    Initial commit of html, css and js files (prior to the start of application development)

---
+++ logic.js
@@ -0,0 +1,20 @@
+/* Javascript logic for "Mark's Coin Cap" application */
+
+// Switch on enhanced error checking by:
+// 1. Enabling Javascript 'strict mode'
+"use strict"   
+// 2. Enabling VS Code's Error Checking [by adding comment '// @ts-check' ] 
+// @ts-check
+
+
+// Define and Construct API URLs (for Coingeko API calls )
+const BASE_URL = "https://api.coingecko.com/api/v3"
+      
+
+
+
+
+// FUNCTIONS
+
+ 
+
