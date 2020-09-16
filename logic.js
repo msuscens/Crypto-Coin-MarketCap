5e8111eef7304c8fe48f18e3e69231c8d0a0dd4d
Mark Suscens <mark.suscens@gmail.com>
2020-09-16 14:01:30 +0100

    In data.js: Added functions to return a hardcoded dummy dataset consiting of 3 coins only. This dummy coin data is in the form that a CoinGeko 'coins/markets' API call would return.  A further function takes this coin data and constructs a data object that contains only the data relevant to the application. In logic.js: This function is called (to obtain the relevant coin data) and the returned coin data is used to populate/update the coin table and page footer.

--- logic.js
+++ logic.js
@@ -2,19 +2,94 @@
 
 // Switch on enhanced error checking by:
 // 1. Enabling Javascript 'strict mode'
-"use strict"   
+"use strict"
 // 2. Enabling VS Code's Error Checking [by adding comment '// @ts-check' ] 
 // @ts-check
 
+// Obtain the Coin Data
+// Using dummy hardcoded coin data for now (albeit in the format that CoinGeko API would return).
+// TODO: Replace dummyGetCoins() with function that obtains data via coinGeko API calls:
+//              const defaultCurrency = "USD"
+//              let coinObjectList = getCoins( defaultCurrency )
+let coinObjects = dummyGetCoins()
+console.log("coinObjects = ", coinObjects)
 
-// Define and Construct API URLs (for Coingeko API calls )
-const BASE_URL = "https://api.coingecko.com/api/v3"
-      
+$("document").ready(function () {
 
+    // Populate the page with the coin data
+    updateCoinTable(coinObjects)
+    updatePageFooter(coinObjects)
+
+})
 
 
 
 // FUNCTIONS
 
- 
+function updateCoinTable(myCoins) {
+    // Fill the table with coin data, one row for each coin
 
+    let htmlCoinTableID = $("#myCoinTableBody")
+
+    htmlCoinTableID.html("")
+    $.each(myCoins, function (index, coin) {
+        let coinTableRowHTML = constructTableRowHtml(coin)
+        console.log(index + " coinTableRowHTML: ", coinTableRowHTML)
+        // add row of coin data to the table
+        htmlCoinTableID.append(coinTableRowHTML)
+    })
+}
+
+
+function constructTableRowHtml(coin) {
+    // Create the HTML string for a coin's table row, using the given coin data.
+    // TODO: Add currency & percentage symbols, and round/format figures
+
+    let coinRowHtml = `<tr>
+                             <td>` + coin.market_cap_rank + `</td>
+                             <td>` + coin.name + `</td>
+                             <td>` + coin.current_price + `</td>
+                             <td>` + coin.price_change_percentage_24h_in_currency + `</td>
+                             <td>` + "Unknown" + `</td>
+                             <td>` + "Unknown" + `</td>
+                             <td>` + coin.market_cap + `</td>
+                             <td>` + "Price-Graph-Here..." + `</td>
+                            </tr>`
+
+    return coinRowHtml
+}
+
+
+
+function updatePageFooter(myCoins) {
+    // Update the HTML string of the table footer (with lastest coin data obtained).
+    // TODO: Add currency and formating of marketcap figure and last update day/time.
+    //        Eg. Total Market Cap: $325,118,468,342
+    //            Last updated: Tue, 08 Sep 2020 14:25:18 UTC
+
+    let htmlFooterID = $("#myPageFooter")
+    htmlFooterID.html("")
+
+    let totalMarketCap = sumMarketCap(myCoins)
+
+    // We'll use the first coin returned last updated time as the
+    // last updated time for all coins
+    // TODO: Consider if there's a better approach for overall last updated time!?
+    let lastUpdated = myCoins[0].last_updated
+
+    let footerHtml = `<p><strong>Total Market Cap: ` + totalMarketCap +
+        `</strong></p>
+                        <p>Last updated: ` + lastUpdated + `</p>`
+
+    htmlFooterID.append(footerHtml)
+
+}
+
+
+function sumMarketCap(myCoins) {
+    let sum = 0;
+    for (let coin of myCoins) {
+        sum += coin.market_cap
+    }
+    return sum
+}
