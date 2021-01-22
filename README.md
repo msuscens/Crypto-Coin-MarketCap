33a68bef2302b844592493f09df81cfb034bfee6
Mark Suscens <mark.suscens@gmail.com>
2021-01-22 12:18:59 +0000

    Reset table’s sort icons upon paging
    Specifically:
    - Extended and fixed code so that when user sorts a table (by column) and then pages (uses next tor previous buttons) the table’s column sort icons a are reset.  [tableFunctions.js]
    - Set coin and exchange table’s sort icon in first column to unsorted upon initial data load [index.html]
    
    Miscellaneous
    - Updated Readme, adding section on use of the generic table functions (in this application and for reuse in other applications)

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 Crypto-Coin-MarketCap
 
-My Coinmarketcap.com inspired project:  The home (index) page show coin price and exchange data, and 
-the coin details page displays a price graph for the selected coin and other coin statistics.
-Users can sort the Coins and Exchanges table by column.  Clicking on a row in the coin table, takes
-you to the coin details page, or alternatively use the coin search. There's a currency selector for both the coins table and on the coin details page. Live coin data is obtained via CoinGeko API calls.
+My Coinmarketcap.com inspired project:  The home (index) page show coin price and exchange data, and the coin details page displays a price graph for the selected coin
+(plus other coin statistics).
+Users may sort the tables by column (coins, exchanges and trading-pairs table).
+Clicking on a row in the coin table (or exchanges table), invokes coin details page
+(or exchange details page).  Alternatively use the 'coin search' to go to the coin 
+details page (or use the 'exchange search' on exchange details page). There's a
+currency selector for the coins table and the coin details page.
+Live coin and exchange data is obtained via CoinGeko API calls.
 
-This project was developed by Mark Suscens as a learning project (to employ HTML, CSS, Bootstrap, JQuery and JavaScript).
+This personal project is being developed by Mark Suscens as a learning project
+(employing HTML, CSS, Bootstrap, JQuery and JavaScript).
+
+Project Status:  The project is under development but is functional, using live API data.
+
+Next Steps:
+  i) Refine styling of the index and coin details page
+ ii) Add an Exchange Search onto the home page - that takes user to 'exchange details' page.
 
 The code is organised as follows:
 - interfaceAPI.js   : Obtains the application data (via API calls), handling missing data and
@@ -20,19 +31,20 @@ The code is organised as follows:
 
 - styles.css        : The css styles for the project, i.e the index and coin pages
 
-- tableFunctions.js : Event handling functions used for all tables in application for table
-                    column sorting, paging and table data reload (refresh)
+- tableFunctions.js : Generic Event handling functions used for all tables in application
+                      for table column sorting, paging and table data reload (refresh)
+                      - see below for dcumentaion on use
 
-- componentClassess.js      : Custom dynamic components. Includes the coin search, exchange search and
-                              currency selector components (used on the index, coin, and exchange pages)
-- searchComponentClass.js   : Search Component Super Class, that is parent object for the coin search 
-                              component
+- componentClassess.js      : Custom dynamic components. Includes the 'coin search',
+                              'exchange search', and 'currency selector' components
+                              (used on the index, coin, and exchange pages)
+- searchComponentClass.js   : Search Component Super Class for the 'coin search'
+                              and 'exchange search' component classes
 - searchComponentStyles.css : The css styles for the Serach Component super class
 
 
-
-
-- helperFunctions.js : Functions supporting page logic implementaion (but are not application specific).
+- helperFunctions.js        : Functions supporting page logic implementaion
+                              (but are not application specific).
 
 
 Note: Requires local server to be running, eg. in terminal window:
@@ -41,14 +53,48 @@ Note: Requires local server to be running, eg. in terminal window:
   (Or run from VS Code using LiveServer)
 
 
-Project Status:  The project is under development but is functional, using live API data.
+___________________________________________________________________________
 
-Next Steps:
-  i) Refine styling of the index and coin details page
- ii) Add an Exchange Search onto the home page - that takes user to 'exchange details' page.
-iii) Consider creating a new git/gitHub repository for the 'Search Component' superclass,
-     together with developer documentation on implementation to create own derived search components.
-    (A rough first draft of such documemtation is in APPENDIX below.)
+DEVELOPER DOCUMENTATION FOR RE-USABLE CODE:
+
+Generic Table Sorting, Paging and Reload event handlers [tableFunctions.js]
+
+Purpose: Allows a single event handler to be employed (one for each of these operations) 
+        across all the applications tables.
+
+Implementation:  To use in other applications, requires that:
+1. Each table's data and metadata is held by a HTML element in two properties 
+   created on the element (called 'tableData' and 'tableMetadata'):
+      'tableData' holds an object with data attributes for each table columns
+      'tableMetaData' object for each table must include attributes of:
+          rowsPerPage:            set to number of rows in table 
+          currentPageNumber:      typically initially set to 1
+          loadTableDataFunction:  name of function to be called to reload data
+                                  (based upon 'tableMetaData') and to then
+                                  store it in 'tableData' property
+          populateTableFunction:  name of function to be called to put the
+                                  'tableData' into the table
+          
+ 2. The HTML element (that holds the table data) must be a parent of both
+    the table and the tables paging/ reload buttons.  And this HTML element
+     must be given an id (which we'll refer to below as idHtmlElementWithTableData)
+
+ 3. The each sort column table header (<th>), and each paging/refresh button has:
+    a) their html "onclick" event handler as the relevant one of:
+         i)   sortTableRows(event, idHtmlElementWithTableData)
+         ii)  loadNextPageIntoTable(idHtmlElementWithTableData)
+         iii) loadPreviousPageIntoTable(idHtmlElementWithTableData)
+         iv)  loadFirstPageIntoTable(idHtmlElementWithTableData)
+    b) each of the html tables column headers <th> must be given an attribute 
+      'dataObjectAttribute' that must be set to the table data object attribute's
+      name that will populate this table column (ie. it maps the table column to
+      the table data).
+
+
+TODO : *** Add similar documention for the data layer (InterfaceAPI) here ***
+
+TODO : *** Add similar documention for the Search Component Super Class   ***
+       *** and derived search components here (rework draft below)        ***
 
 ___________________________________________________________________
 
--- tableFunctions.js
+++ tableFunctions.js
@@ -1,16 +1,24 @@
 //
-// TABLE SORTING EVENT HANDLERS
+// Generic Table sorting, paging and refresh event handlers.
+//
+// Allows a single event handler to be employed (one for each of these operations) 
+// across all the applications tables.  See README for details.
+//
+
+//
+// GENERIC TABLE SORTING - COLUMN SORT EVENT HANDLER
 //
 function sortTableRows(event, idHtmlElementWithTableData) {
   try {
+    console.log("In sortTableRows():")
     // Retreive table data and table's metadata
     const tableData = $(`#${idHtmlElementWithTableData}`).prop("tableData")
     const tableMetadata = $(`#${idHtmlElementWithTableData}`).prop("tableMetadata")
     if (!tableData || !tableMetadata) throw ("Asked to sort with unknown table data/metadata")
 
+    // Sort table data
     const currentSortOrder = $(event.currentTarget).prop("order")
     let newSortOrder
-
     if ((currentSortOrder == "descending") || (currentSortOrder == "ascending")) {
         // Previously sorted on this column, so simply reverse data order
         tableData.reverse()
@@ -19,7 +27,7 @@ function sortTableRows(event, idHtmlElementWithTableData) {
     else {  // Not previously sorted
         newSortOrder = "descending"  // default for sort
 
-        // Prepare a compare function (specific to sort column's data type, and the sort order)
+        // Make a compare function (specific to sort column's data type, and sort order)
         const dataAttribute = $(`#${event.currentTarget.id}`).attr("dataObjectAttribute")
         const functionBody = createCompareFunctionBody(tableData[0], dataAttribute, newSortOrder)
         const compareFunction = Function("a, b", functionBody)
@@ -27,67 +35,51 @@ function sortTableRows(event, idHtmlElementWithTableData) {
         // Sort the table data
         tableData.sort(compareFunction)
     }
-    // Store the sorted table data
+
+    // Store sorted table data
     $(`#${idHtmlElementWithTableData}`).prop("tableData", tableData)
 
     // Update the table (with sorted data)
-    setColumnHeadersSortOrder(event.currentTarget, newSortOrder)
-
     tableMetadata.populateTableFunction(tableData)
+    setColumnHeadersSortOrder(event.currentTarget.id, newSortOrder)
   }
   catch (errMsg) {
     throw("In sortTableRows(event, idHtmlElementWithTableData): " + errMsg)
   }
 }
-  
-function setColumnHeadersSortOrder(currentTargetColumn, newSortOrder) {
+ 
+function setColumnHeadersSortOrder(idSortedColumn, newSortOrder) {
+// Update table column header's sort icons and 'order' attribute
+// (according to columns newSortOrder: "ascending", "descending")
   try {
     const unsortedIcon = '<i class="fas fa-sort"></i>'
     const sortedAscendingIcon = '<i class="fas fa-sort-up"></i>'
     const sortedDescendingIcon = '<i class="fas fa-sort-down"></i>'
 
     // Set sort icons in table column headers
-    const sortOrderIcon = (newSortOrder === "ascending") ? sortedAscendingIcon : sortedDescendingIcon
-    $("[name='sortColumn']").find("span").html( unsortedIcon )
-    $("#" + currentTargetColumn.id).find("span").html( sortOrderIcon )
+    const sortOrderIcon = (newSortOrder === "ascending") ? sortedAscendingIcon :
+                        ((newSortOrder === "descending") ? sortedDescendingIcon :
+                                                          unsortedIcon)
+    $("#"+idSortedColumn).find("span").html( sortOrderIcon )
+    $("#"+idSortedColumn).siblings("th[name='sortColumn']").find("span").html(unsortedIcon)
 
     // Set 'order' property on table column headings
-    $(currentTargetColumn).prop("order", newSortOrder)
-    $(currentTargetColumn).siblings().removeProp("order")
-    
+    if ((newSortOrder === "ascending") || (newSortOrder === "descending")) {
+      $("#"+idSortedColumn).prop("order", newSortOrder)
+    }
+    else {
+      $("#"+idSortedColumn).removeProp("order")
+    }
+    $("#"+idSortedColumn).siblings("th[name='sortColumn']").removeProp("order")
   }
   catch (errMsg) {
-    throw("In setColumnHeadersSortOrder(currentTargetColumn, newSortOrder): " + errMsg)
+    throw("In setColumnHeadersSortOrder(idSortedColumn, newSortOrder): " + errMsg)
   }
 }
 
-/*  This function refactored above- so that it takes currentTarget rather than an event
-    Refactored as part of anlysis into reusing this function to reset the comlmn sort icons,
-    with idea of calling this function with tables first <th> element
-    KEEP BOTH VERSIONS FOR NOW UNTIL THIS ISSUE IS RESOLVED
-function setColumnHeadersSortOrder(event, newSortOrder) {
-  try {
-    const unsortedIcon = '<i class="fas fa-sort"></i>'
-    const sortedAscendingIcon = '<i class="fas fa-sort-up"></i>'
-    const sortedDescendingIcon = '<i class="fas fa-sort-down"></i>'
-
-    // Set sort icons in table column headers
-    const sortOrderIcon = (newSortOrder === "ascending") ? sortedAscendingIcon : sortedDescendingIcon
-    $("[name='sortColumn']").find("span").html( unsortedIcon )
-    $("#" + event.currentTarget.id).find("span").html( sortOrderIcon )
-
-    // Set 'order' property on table column headings
-    $(event.currentTarget).prop("order", newSortOrder)
-    $(event.currentTarget).siblings().removeProp("order")
-  }
-  catch (errMsg) {
-    throw("In setColumnHeadersSortOrder(event, newSortOrder): " + errMsg)
-  }
-}
-*/
 
 //  
-// TABLE PAGING - EVENT HANDLERS
+// GENERIC TABLE PAGING - NEXT ANDF PREVIOUS EVENT HANDLERS
 //
 function loadNextPageIntoTable(idHtmlElementWithTableData) {
   try {
@@ -108,17 +100,9 @@ function loadNextPageIntoTable(idHtmlElementWithTableData) {
     // Display next page of data in table
     tableMetadata.loadTableDataFunction()
     
-    // Reset the column sort icons
-    // *** TODO  ***
-    // **** Issue:  How to call from here: setColumnHeadersSortOrder(currentTargetColumn, "ascending")
-    // ****         where currentTargetColumn is the first column <th> target
-    // *** Posibilities to solve:
-    // **** 1. Pass in event to this EH function and then  naviagte DOM to table's first column <th>
-    // ****    But how and is this a good method? Needs to be generic/reusable on all tables!!
-    // **** 2. Save the table column headers id into the table metaData (that this function retreives)
-    // ****    But this adds size/complexity to the metadata that is needed for each table.
-    // **** 3. Some other solution that is elegant, generic ! ???  
-
+    // Reset the column sort order and icons 
+    const idFirstSortColumn = $(`#${idHtmlElementWithTableData} th[name='sortColumn']:first`)[0].id
+    setColumnHeadersSortOrder(idFirstSortColumn, "unsorted")
   }
   catch (errMsg) {
     throw("In loadNextPageIntoTable(idHtmlElementWithTableData): " + errMsg)
@@ -144,15 +128,18 @@ function loadPreviousPageIntoTable(idHtmlElementWithTableData) {
     // Display previous page of coins in table
     tableMetadata.loadTableDataFunction()
 
-    // Reset the column sort icons
-    // *** TODO  ***
-
+    // Reset the column sort order and icons 
+    const idFirstSortColumn = $(`#${idHtmlElementWithTableData} th[name='sortColumn']:first`)[0].id
+    setColumnHeadersSortOrder(idFirstSortColumn, "unsorted")
   }
   catch (errMsg) {
     throw("In loadPreviousPageIntoTable(idHtmlElementWithTableData): " + errMsg)
   }
 }
 
+//  
+// GENERIC TABLE DATA RELOAD / REFRESH - EVENT HANDLER
+//
 function loadFirstPageIntoTable(idHtmlElementWithTableData) {
   try {
     // Reset page number to first page
@@ -166,13 +153,11 @@ function loadFirstPageIntoTable(idHtmlElementWithTableData) {
 
     tableMetadata.loadTableDataFunction()
 
-    // Reset the column sort icons
-    // *** TODO  ***
-
+    // Reset the column sort order and icons 
+    const idFirstSortColumn = $(`#${idHtmlElementWithTableData} th[name='sortColumn']:first`)[0].id
+    setColumnHeadersSortOrder(idFirstSortColumn, "unsorted")
   }
   catch (errMsg) {
     throw("In loadFirstPageIntoTable(idHtmlElementWithTableData): " + errMsg)
   }
 }
-
-
